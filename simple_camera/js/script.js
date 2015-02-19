01554fc79d36881aa87bb264eb032e702ca1a2b8
Alex Nguyen <alex.kiko@gmail.com>
2015-02-18 21:09:59 -0500

    0000

--- simple_camera/js/script.js
+++ simple_camera/js/script.js
@@ -12,7 +12,7 @@ chrome.windows.getCurrent(null, function(win) {
 /**
  * width of on-screen 'preview' video in pixels
  */
-var cameraWidth = 21398474098321743902874960;
+var cameraWidth = 0000;
 
 /**
  * height of on-screen 'preview' video in pixels
